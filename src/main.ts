fd4e1fd92c78e3091e3f3f6230b5813bc0dc5529
33357 <im33357dr@gmail.com>
2024-03-12 12:32:51 +0800

    0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,77 @@
-import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
-import { TOKEN_PROGRAM_ID ,getTokenMetadata} from '@solana/spl-token';
+import { Connection, PublicKey } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { Umi, PublicKey as _PublicKey } from "@metaplex-foundation/umi";
+import {
+  Metadata,
+  fetchDigitalAsset,
+} from "@metaplex-foundation/mpl-token-metadata";
 
-// 创建与Solana网络的连接
-const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
-// 指定要查询余额的账户地址
-const accountAddress = 'oK3iLEfh2rqrK9HxARoqmxGRhZyW7yDJsgmUjdmWhyj';
-// 将字符串地址转换为PublicKey
-const publicKey = new PublicKey(accountAddress);
-// 异步函数来获取并打印账户余额
-const getBalance = async () => {
-    try {
-        const balance = await connection.getBalance(publicKey);
-        console.log(`账户余额: ${balance / Math.pow(10, 9)} SOL`);
-    } catch (error) {
-        console.error(error);
-    }
-};
+class Relation {
+  umi: Umi;
+  connection: Connection;
 
-const getAllTokenBalances = async () => {
-    try {
-        // 获取账户的所有代币账户信息
-        const tokenAccounts = await connection.getParsedTokenAccountsByOwner(publicKey, { programId: TOKEN_PROGRAM_ID });
-        for (const { account } of tokenAccounts.value) {
-            const tokenBalance = account.data.parsed.info.tokenAmount.uiAmountString;
-            const tokenMint = account.data.parsed.info.mint;
-            console.log(`代币Mint地址: ${tokenMint}, 余额: ${tokenBalance}`);
-            console.log(await getTokenInfo(tokenMint))
-        }
-    } catch (error) {
-        console.error('Error fetching token balances:', error);
-    }
-};
+  constructor(url: string) {
+    this.umi = createUmi(url);
+    this.connection = new Connection(url);
+  }
 
-const getTokenInfo = async (mintAddress: string) => {
-    // 尝试获取代币的元数据（名称和符号）
+  // 获取账户余额
+  async getBalance(accountAddress: string) {
+    const publicKey = new PublicKey(accountAddress);
+    const balance = await this.connection.getBalance(publicKey);
+    return (balance / Math.pow(10, 9)).toString();
+  }
+
+  // 获取账户的所有代币账户信息
+  async getAllTokenData(accountAddress: string) {
     try {
-        const token = await getTokenMetadata(
-            connection,
-            new PublicKey(mintAddress),
-            undefined,
-            TOKEN_PROGRAM_ID
-        );
-        return token;
+      const publicKey = new PublicKey(accountAddress);
+      const tokenAccounts = await this.connection.getParsedTokenAccountsByOwner(
+        publicKey,
+        { programId: TOKEN_PROGRAM_ID }
+      );
+      const dataList: {
+        balance: string;
+        metaData: Metadata | null;
+      }[] = [];
+      const balance = await this.getBalance(accountAddress);
+      dataList.push({
+        balance,
+        metaData: null,
+      });
+      for (const { account } of tokenAccounts.value) {
+        const balance = account.data.parsed.info.tokenAmount.uiAmountString;
+        const mintAddress = account.data.parsed.info.mint;
+        const data = {
+          balance,
+          metaData: await this.getTokenMetaData(mintAddress),
+        };
+        dataList.push(data);
+      }
+      return dataList;
     } catch (error) {
-        console.log('Error fetching token info:', error);
+      console.error("Error fetching token balances:", error);
     }
+  }
+
+  // 尝试获取代币的元数据
+  async getTokenMetaData(mintAddress: string) {
+    const asset = await fetchDigitalAsset(
+      this.umi,
+      new PublicKey(mintAddress) as unknown as _PublicKey
+    );
+    return asset.metadata;
+  }
 }
 
-// 调用函数
-getBalance();
-//getTokenBalance();
-getAllTokenBalances()
+async function main() {
+  const relation = new Relation("https://api.devnet.solana.com");
+  console.log(
+    await relation.getAllTokenData(
+      "A4pPdjPaDMTK8dbSN5eyPzUWuCpJmAZkDirm87gVSqHf"
+    )
+  );
+}
+
+main();
